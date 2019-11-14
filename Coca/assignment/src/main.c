c6d4b6ee7db72b65f7ac5382cddad376739ed111
Exjp <exilfaker@gmail.com>
2019-11-14 11:53:43 +0100

    -h was printing 2 times the help

--- Coca/assignment/src/main.c
+++ Coca/assignment/src/main.c
@@ -70,7 +70,7 @@ int main(int argc, char* argv[]){
             case ('h'):
             {
                 usage();
-                break;
+                return 0;
             }
             case ('v'):
             {
